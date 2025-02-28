527a6755f5a569e5fed1752a4f492b80bcb531ab
mayank-kumar03 <mmayanktripathi897@gmail.com>
2025-02-28 23:47:52 +0530

    Frontend update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { CreateTodo } from './components/CreateTodo'
+import { Todos } from './components/Todos'
+//import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+  fetch("http://localhost:8000/todos")
+  .then(async function (res) {
+    const json=await res.json();
+    setTodos(json.todos)
+    
+  })
 
   return (
     <>
-     <h1>hi lets start
-      
-     </h1>
+    <CreateTodo/>
+    <Todos todos={todos}/>
     </>
   )
 }
